45926414db996fb4c6859ee7b1f1b00883002690
YuriyOhii <uaogiy@gmail.com>
2024-01-03 14:27:13 +0200

    create env vars

--- server.js
+++ server.js
@@ -5,13 +5,12 @@ import mongoose from "mongoose";
 import app from "./app.js";
 // double 2
 
-const DB_HOST =
-  "mongodb+srv://uaogiy:uEXvUieoQhFXVwDa@cluster0.cuxjisd.mongodb.net/sample_movies?retryWrites=true&w=majority";
+const {DB_HOST, PORT}= process.env;
 mongoose
   .connect(DB_HOST)
   .then(() =>
     app.listen(3000, () => {
-      console.log("Server is running on PORT 3000");
+      console.log(`Server is running on PORT ${PORT}`);
     })
   )
   .catch((er) => {
